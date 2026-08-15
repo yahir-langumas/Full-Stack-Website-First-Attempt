11d69b8b68a84d5b815e4a917fba933e0753f25a
Yahir <yahirlangumas@gmail.com>
2026-08-15 12:41:06 -0500

    Added Correct URL to fetch data from API. Add general information about Pokemons. Next step is to make migrations once complete.

--- pokedata/management/commands/import_species.py
+++ pokedata/management/commands/import_species.py
@@ -4,32 +4,41 @@ import requests
 from django.core.management.base import BaseCommand
 
 class Command(BaseCommand):
-    limit = 1 
-    poke_url = f"https://pokeapi.co/api/v2/pokemon/?offset=20&limit={limit}"
-    while limit <= 1351: 
-        response = requests.get(poke_url)
-        data = response.json()
-        for pokemon in data["results"]:
-            pokemon_response = requests.get(pokemon["url"])
-            pokemon_data = pokemon_response.json()
-            name = pokemon_data["name"]
-            height = pokemon_data["height"]
-            weight = pokemon_data["weight"]
-            base_experience = pokemon_data["base_experience"]
-            types = [t["type"]["name"] for t in pokemon_data["types"]]
-            abilities = [a["ability"]["name"] for a in pokemon_data["abilities"]]
-            stats = {s["stat"]["name"]: s["base_stat"] for s in pokemon_data["stats"]}
+    species_id = 1
+    while species_id <= 1028:
+        poke_api_response = requests.get(f"https://pokeapi.co/api/v2/pokemon/{species_id}/")
+        pokemon_data = poke_api_response.json()
+        pokemon_name = pokemon_data["name"]
+        # print("Pokemon Name: ", pokemon_name)
+
+        species_id +=1
+    # limit = 1 
+    # poke_url = f"https://pokeapi.co/api/v2/pokemon/?offset=20&limit={limit}"
+    # while limit <= 1351: 
+        # response = requests.get(poke_url)
+        # data = response.json()
+        # for pokemon in data["results"]:
+            # pokemon_response = requests.get(pokemon["url"])
+            # pokemon_data = pokemon_response.json()
+            # name = pokemon_data["name"]
+            # height = pokemon_data["height"]
+            # weight = pokemon_data["weight"]
+            # base_experience = pokemon_data["base_experience"]
+            # types = [t["type"]["name"] for t in pokemon_data["types"]]
+            # abilities = [a["ability"]["name"] for a in pokemon_data["abilities"]]
+            # stats = {s["stat"]["name"]: s["base_stat"] for s in pokemon_data["stats"]}
             
             # Save the data to the database
-            from pokedata.models import Pokemon
-            Pokemon.objects.create(
-                name=name,
-                height=height,
-                weight=weight,
-                base_experience=base_experience,
-                types=types,
-                abilities=abilities,
-                stats=stats
-            )
-        limit += 1
-        poke_url = f"https://pokeapi.co/api/v2/pokemon/?offset={limit}&limit={limit}"
+            # from pokedata.models import Pokemon
+            # Pokemon.objects.create(
+                # name=name,
+                # height=height,
+                # weight=weight,
+                # base_experience=base_experience,
+                # types=types,
+                # abilities=abilities,
+                #stats=stats
+            #)
+        # limit += 1
+        # poke_url = f"https://pokeapi.co/api/v2/pokemon/?offset={limit}&limit={limit}"
+        
--- pokedata/models.py
+++ pokedata/models.py
@@ -2,6 +2,16 @@ from django.db import models
 
 # Create your models here.
 
+class Species(models.Model): 
+    name = models.CharField(max_length=100)
+    id = models.IntegerField()
+    # height = models.IntegerField()
+    # weight = models.IntegerField()
+    # base_experience = models.IntegerField()
+    types = models.JSONField()
+    abilities = models.JSONField()
+    stats = models.JSONField()
+    sprite = models.URLField()
 class Todo(models.Model):
     title = models.CharField(max_length=100)
     completed = models.BooleanField(default=False)
