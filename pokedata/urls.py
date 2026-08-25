649b2ac09abe0c353839d27fb465a57e947a7d09
Yahir <yahirlangumas@gmail.com>
2026-08-25 00:34:36 -0500

    Finish front-end using HTML. Created the home page and Pokemon Table page. Need to finish damage calculator and team builder.

--- pokedata/urls.py
+++ pokedata/urls.py
@@ -1,6 +1,9 @@
-from django.urls import path 
+from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home, name = "home")
-] 
+    path('', views.home, name='home'),
+    path('pokemon-table/', views.pokemon_table, name='pokemon_table'),
+    path('team-builder/', views.team_builder, name='team_builder'),
+   
+]
--- pokedata/views.py
+++ pokedata/views.py
@@ -1,4 +1,20 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
+from .models import Species
+
 
 def home(request):
-    return HttpResponse("Hello World!")
+    species_count = Species.objects.count()
+    return render(request, "home.html", {"species_count": species_count})
+
+
+def pokemon_table(request):
+    species_list = Species.objects.all().order_by("pokedex_id")
+    return render(request, "pokemon_table.html", {"species_list": species_list})
+
+
+def team_builder(request):
+    species_list = Species.objects.all().order_by("pokedex_id")
+    return render(request, "team_builder.html", {"species_list": species_list})
+
+
+# your existing todos view stays here, unchanged
