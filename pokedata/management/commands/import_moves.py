616c1425fe6a899f641747fac56005de7a7e9100
Yahir <yahirlangumas@gmail.com>
2026-09-02 16:41:57 -0500

    Started to add parameters to the import_moves, still needs adjusting with formating and what data to add in. Very close!

--- pokedata/management/commands/import_moves.py
+++ pokedata/management/commands/import_moves.py
@@ -8,9 +8,30 @@ class Command(BaseCommand):
         move_id = 1
         max_move_id = 919 
         while move_id <= max_move_id: 
-            poke_api_response = requests.get(f"https://pokeapi.co/api/v2/move/{move_id}")
+            poke_api_response = requests.get(f"https://pokeapi.co/api/v2/move/{move_id}/")
             move_data = poke_api_response.json()
-            Moves.objects.get_or_create() 
+            damage_class = move_data["damage_class"]["name"]
+            type = move_data["type"]["name"]
+            # Make field into a tuple using this: "ailment": move_data["meta"]["ailment"]
+            meta = move_data["meta"]["ailment"]["name"] if move_data["meta"]["ailment"] else None, 
+            move_data["meta"]["category"]["name"], 
+            move_data["meta"]["min_hits"], move_data["meta"]["max_hits"], move_data["meta"]["min_turns"], move_data["meta"]["max_turns"], 
+            move_data["meta"]["drain"], move_data["meta"]["healing"], move_data["meta"]["crit_rate"], move_data["meta"]["ailment_chance"], 
+            move_data["meta"]["flinch_chance"], move_data["meta"]["stat_chance"]
+            Moves.objects.get_or_create(
+                move_id = move_data["id"], 
+                defaults = { 
+                    "name" : move_data["name"], 
+                    "power": move_data["power"], 
+                    "accuracy" : move_data["accuracy"],
+                    "priority": move_data["priority"],
+                    "pp" : move_data["pp"],
+                    "effect_chance": move_data["effect_chance"],
+                    "damage_class" : damage_class, 
+                    "type" : type, 
+                    "meta" : meta
+                }
+            ) 
 
 
             move_id += 1 
--- pokedata/models.py
+++ pokedata/models.py
@@ -16,5 +16,18 @@ class Species(models.Model):
     def __str__(self):
         return self.name
 class Moves(models.Model): 
+    name = models.CharField(max_length=100)
+    move_id = models.IntegerField(unique = True)
+    power = models.IntegerField(null=True)
+    accuracy = models.IntegerField(null=True)
+    effect_chance = models.IntegerField(null=True)
+    pp = models.IntegerField()
+    meta = models.JSONField()
+    stat_changes = models.JSONField(null=True)
+    # Meta refers to the metadata of the move, so attributes like move effect such as "has a 30% chance to paralyze the target"
+    #  or "increases the user's speed by 1 stage" would be stored in this field.
+    priority = models.IntegerField()
+    type = models.CharField(max_length=100)
+    damage_class = models.CharField(max_length=100)
     def __str__(self):
         return self.name
